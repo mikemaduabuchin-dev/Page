92853200ee5084df49737374d2ff27ae039627b6
mikemaduabuchin-dev <mikemaduabuchin@gmail.com>
2026-09-08 17:47:26 +0100

    here

---
+++ assets/second-BcHIClmc.js
@@ -0,0 +1,1 @@
+import{E as e,S as t,c as n,p as r}from"./runtime-core.esm-bundler-Bzlnoy4P.js";import{t as i}from"./QBtn-Lpj119On.js";import{t as a}from"./QPage-HV0PacBE.js";var o={__name:`second`,setup(o){return(o,s)=>(t(),n(a,{class:`flex flex-center`},{default:e(()=>[r(i,{color:`secondary`,to:`/`,label:`Go to Index Page`,"no-caps":``})]),_:1}))}};export{o as default};
